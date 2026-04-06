c055560b33f0c5ddad1ed4b30adcfda6f0d66990
Olamilekan Johnson <ojohns32@my.centennialcollege.ca>
2026-04-06 10:28:07 -0400

    Added database access message

--- olam_quiz4_dotnet/Program.cs
+++ olam_quiz4_dotnet/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello World");
+        Console.WriteLine("Database access has already been done on April 6, 2026 by Olamilekan Johnson");
     }
 }
